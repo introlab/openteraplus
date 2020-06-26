bdd695b9bac38aa07613bb513f23c959444b1143
Dominic Létourneau <doumdi@gmail.com>
2020-06-26 11:14:54 -0400

    Updated build for Mac

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -9,16 +9,21 @@ int main(int argc, char* argv[])
 {
     int rval;
     // Force use of ANGLE instead of Desktop OpenGL - some memory leak may happen with WebRTC on Intel Graphics Cards otherwise.
+#ifdef WIN32
     QApplication::setAttribute(Qt::AA_UseOpenGLES, true);
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
     QApplication::setAttribute(Qt::AA_DisableWindowContextHelpButton, true);
+#endif
 
+#ifdef __APPLE__
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     ClientApp* app = new ClientApp(argc, argv);
 
-    //Set application style
-    #ifndef WIN32 // Don't set style on Windows - it creates some issues with combobox look.
-        //app->setStyle("windows");
-    #endif
+//Set application style
+#ifndef WIN32 // Don't set style on Windows - it creates some issues with combobox look.
+    //app->setStyle("windows");
+#endif
 
 
     //WebEngine default Settings
