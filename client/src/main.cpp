977450ba030baff92e9ed6b20285b1c1ccc8c7dc
SBriere <simon.briere@usherbrooke.ca>
2021-05-20 09:20:30 -0400

    Refs #15 - Mac packaging with working signing and notarization

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -22,8 +22,12 @@ int main(int argc, char* argv[])
 #ifdef __APPLE__
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 #endif
-    //TODO: Remove exposed debugging port in production
+
+#ifdef QT_DEBUG
     qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--autoplay-policy=no-user-gesture-required --remote-debugging-port=22222"); // Allow auto-play feature in webengine without any user interaction to test, for example, the microphone and play sounds
+#else
+    qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--autoplay-policy=no-user-gesture-required"); // Allow auto-play feature in webengine without any user interaction to test, for example, the microphone and play sounds
+#endif
     ClientApp app(argc, argv);
 
 //Set application style
